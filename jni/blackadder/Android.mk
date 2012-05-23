d572663f003edb592eb066f3a0dfed8281fca737
Xinghong Fang <xinghong.fang@gmail.com>
2012-05-23 15:16:44 +0100

    - blackadder related JNI code separated

--- jni/blackadder/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := blackadder
-LOCAL_SRC_FILES := bitvector.cpp blackadder.cpp nb_blackadder.cpp
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
-
-include $(BUILD_SHARED_LIBRARY)
